3fb1a45ddeeca771f03fa08e41b2550e254d0e5f
mahendranath <mahendra1729@yahoo.com>
2021-10-28 19:49:58 +0530

    Code updated

--- src/main/java/demo/camel/Proc.java
+++ src/main/java/demo/camel/Proc.java
@@ -1,13 +1,21 @@
 package demo.camel;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.camel.support.DefaultMessage;
+
+import java.sql.SQLOutput;
 
 public class Proc implements Processor {
 
 	public void process(Exchange exchange) throws Exception {
+
 		System.out.println("Inside Producer");
-		exchange.getOut().setBody((String)exchange.getIn().getBody() + "-by Mahendra Nath");
+		System.out.println("Ths Exchange Identifir is :" +exchange.getExchangeId());
+		System.out.println("Ths Mesage Identifir is : "+ exchange.getMessage().getMessageId());
+		exchange.getMessage().setBody(exchange.getMessage().getBody() + "-by Mahendra Nath");
+
 
 	}
 
